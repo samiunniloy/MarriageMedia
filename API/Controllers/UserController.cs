31e427e8810d724f2958a6b05c064f05fd80c1b6
Samiun Islam <60150274+samiunniloy@users.noreply.github.com>
2025-01-09 14:03:33 +0600

    Initial Upload

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserController(DataContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {   
+            // Task async operation bujhay
+            //Ienumerable<AppUser> er jaygay var o use kora jay
+            //Ienumerable<AppUser> er jaygay List<AppUser> o use kora jay
+            //IEnumerable array type er jnno use hoy
+            var users = await context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")] //api/users/id
+        // {id } er jnno dynamic route use kora jay na dile dynamic route hoy na 
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user == null) {
+                return NotFound("Id doesnt exist");
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace API.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
