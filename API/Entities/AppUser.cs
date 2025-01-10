3bb5c80488ea0f5cf744e61a958590f66e73ba82
samiunniloy <60150274+samiunniloy@users.noreply.github.com>
2025-01-11 00:11:26 +0600

    AppUser update

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,13 @@
-﻿namespace API.Entities
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public required string UserName { get; set; }
+        public required byte[] PasswordHash { get; set; }
+        public required Byte[] PasswordSalt { get; set; }
+
     }
 }
