75455d7bf593361483d3d47f641c232646735af0
Samiun Islam <60150274+samiunniloy@users.noreply.github.com>
2025-01-12 18:29:20 +0600

    update token.services

--- API/Services/TokenServices.cs
+++ API/Services/TokenServices.cs
@@ -5,7 +5,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-namespace API.Services
+namespace API.services
 {
     public class TokenServices(IConfiguration config) : ITokenService
     {
