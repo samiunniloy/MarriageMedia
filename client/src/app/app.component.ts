c984404347410a29cd25defa1389f96495275a8d
Samiun Islam <60150274+samiunniloy@users.noreply.github.com>
2025-01-14 18:25:10 +0600

    mid time push

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import {NavComponent} from './nav/nav.component'; 
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +13,29 @@ import {NavComponent} from './nav/nav.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements  OnInit{
-  http=inject(HttpClient);
+  http = inject(HttpClient);
+  private accountService = inject(AccountService);
   title = 'MarriageMedia';
   users: any;
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers() {
     this.http.get('https://localhost:7198/api/User').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
-    });
+    })
   }
+
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,
+    CommonModule,
+    BsDropdownModule,],
+
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  accountService = inject(AccountService);
   model: any = {};
   login(){
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+       
+      },
+      error: error => console.log(error)
+    })
+  // console.log(this.model);
+  }
+  logout(){
+    this.accountService.logout();
   }
 }
